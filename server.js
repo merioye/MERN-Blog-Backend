345d05d50044f854e3f2f1902f3ce1f8a3d39194
Umair Saleem <saleemakhtar35302@gmail.com>
2022-11-08 09:06:11 +0500

    solving cookie issue

--- server.js
+++ server.js
@@ -26,14 +26,6 @@ mongoose
 app.use(() => {
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Origin", process.env.CLIENT_APP_URL);
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
-    );
-    res.header(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-    );
     next();
 });
 app.use(express.static(uploadsPath));
