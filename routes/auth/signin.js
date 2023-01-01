394c6ff07272d2c775677dd1f90f26f7d2a24047
Umair Saleem <saleemakhtar35302@gmail.com>
2023-01-01 16:38:00 +0500

    added example env file

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -4,45 +4,48 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 router.post("/signin", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).json({
-                message: "All fields are required",
-            });
-        }
-
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({
-                message: "Invalid credentials",
-            });
-        }
-
-        const isMatched = await bcrypt.compare(password, user.password);
-        if (!isMatched) {
-            return res.status(401).json({
-                message: "Invalid credentials",
-            });
-        }
-
-        const accessToken = await jwt.sign(
-            { id: user._id },
-            process.env.TOKEN_SECRET
-        );
-        res.cookie("accessToken", accessToken, {
-            httpOnly: true,
-            expires: new Date(Date.now() + 86400000),
-        });
-
-        res.status(200).json({
-            message: "Logged in",
-        });
-    } catch (e) {
-        res.status(500).json({
-            message: e,
-        });
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
     }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const accessToken = await jwt.sign(
+      { id: user._id },
+      process.env.TOKEN_SECRET
+    );
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+      expires: new Date(Date.now() + 86400000),
+    });
+
+    res.status(200).json({
+      message: "Logged in",
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: e,
+    });
+  }
 });
 
 module.exports = router;
